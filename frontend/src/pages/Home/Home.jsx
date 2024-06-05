20055486783e8ad501d567c451f716c81b95ff4a
El Bahloul El Hassan <el-hassan.el-bahloul@student-cs.fr>
2024-06-05 23:53:07 +0200

    adding dropdown search

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,30 +1,78 @@
-import logo from './logo.svg';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { useFetchMovies } from './useFetchmovies';
 import Movie from '../../components/movie';
+import { Dropdown } from 'primereact/dropdown';
+import { Button } from 'primereact/button';
+import 'primereact/resources/themes/saga-blue/theme.css'; // PrimeReact theme
+import 'primereact/resources/primereact.min.css'; // PrimeReact core CSS
+import 'primeicons/primeicons.css'; // PrimeIcons
 import './Home.css';
 
 function Home() {
-  const [movieName, setMovieName] = useState("");
-  const { movies} = useFetchMovies();
+  const [searchQuery, setSearchQuery] = useState("");
+  const { movies } = useFetchMovies();
+  const navigate = useNavigate();
+
+  const handleSearchChange = (event) => {
+    setSearchQuery(event.target.value);
+  };
+
+  const handleSearchSubmit = () => {
+    // Find the corresponding movie object based on the search query
+    const matchingMovies = Object.values(movies).filter(movie =>
+      movie.title.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+    if (matchingMovies.length > 0) {
+      navigate(`/movieDetails/${matchingMovies[0].id}`);
+    }
+  };
+
+ // Filter dropdown options based on search query
+  const dropdownOptions = Object.values(movies)
+  .filter(movie => movie.title.toLowerCase().includes(searchQuery.toLowerCase()))
+  .map(movie => ({
+    label: movie.title,
+    value: movie.title,
+   }));
+
+  const handleDropdownChange = (e) => {
+    setSearchQuery(e.value); // Update search query based on selected movie title
+    const selectedMovie = Object.values(movies).find(movie => movie.title === e.value);
+    if (selectedMovie) {
+      navigate(`/movieDetails/${selectedMovie.id}`);
+    }
+  };
 
   return (
     <div className="App">
       <header className="App-header">
-        Top Films
+        Welcome to MovieMuse
       </header>
-      <input
-      value = {movieName}
-      onChange = {(event) => setMovieName(event.target.value )}/>
-      {Object.values(movies).filter((el = Object.values(movies)) => el.title.toLowerCase().includes(movieName.toLowerCase())).length > 0 ? (
-      <ul className='App-movies'>
-        {Object.values(movies).filter((el = Object.values(movies)) => el.title.toLowerCase().includes(movieName.toLowerCase())).map((movie, index) => (
-          <Movie key={index} movie={movie}/>
-          
-        ))}
-      </ul>
-      ):(
-      <p>No results found for {movieName}</p>
+      <div>
+        <Dropdown 
+          className='custom-dropdown'
+          value={searchQuery}
+          options={dropdownOptions} 
+          onChange={handleDropdownChange} 
+          editable 
+          placeholder="Search for a movie..." 
+        />
+        <Button 
+          label="Search" 
+          icon="pi pi-search" 
+          onClick={handleSearchSubmit}
+          className="p-button-outlined"
+        />
+      </div>
+      {Object.values(movies).filter(movie => movie.title.toLowerCase().includes(searchQuery.toLowerCase())).length > 0 ? (
+        <ul className='App-movies'>
+          {Object.values(movies).filter(movie => movie.title.toLowerCase().includes(searchQuery.toLowerCase())).map((movie, index) => (
+            <Movie key={index} movie={movie}/>
+          ))}
+        </ul>
+      ) : (
+        <p>No results found for {searchQuery}</p>
       )}
     </div>
   );
