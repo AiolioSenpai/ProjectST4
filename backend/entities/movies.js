0798409719676ba98a65f26cfa32041b81cbaa09
Houssem Guermazi <houssem.guermazi@student-cs.fr>
2024-06-05 19:48:22 +0200

    fixing changes in movie.js

--- backend/entities/movies.js
+++ backend/entities/movies.js
@@ -1,32 +1,57 @@
-import typeorm, { Any } from 'typeorm';
+import typeorm from 'typeorm';
 
-const MovieDB = new typeorm.EntitySchema({
-  name: 'MovieDB',
+const Movie = new typeorm.EntitySchema({
+  name: 'Movie',
   columns: {
     id_movie: {
       primary: true,
       type: Number,
+      generated: true,
     },
     title: {
       type: String,
     },
-    vote_average: {
-      type: "float",
-    },
-    poster_path: {
-      type: String,
-    },
-    genre_ids: {
-      type: "int",
-      array: true,
-    },
-    overview: {
-      type: String,
-    },
-    release_date: {
-      type: String,
-    },
+    release_date: { 
+      type: String
+     },
+     description:{
+      type: String
+     },
+    trailer: { 
+      type: String
+     },
+     image: { 
+      type: String
+     },rating_tmdb:{
+      type:String    }
+     ,
   },
-});
+  relations: {
+    movie_genre: {
+      type: 'many-to-many',
+      target: 'Genre',
+      joinTable: {
+          name: 'movie_genre', // Name of the join table
+          joinColumn: { name: 'id_movie', referencedColumnName: 'id_movie' },
+          inverseJoinColumn: { name: 'id_genre', referencedColumnName: 'id_genre' },
+      },
+  },
+    actors: {
+      type: 'many-to-many',
+      target: 'Actor',
+      joinTable: {
+          name: 'cast', // Name of the join table
+          joinColumn: { name: 'id_movie', referencedColumnName: 'id_movie' },
+          inverseJoinColumn: { name: 'id_actor', referencedColumnName: 'id_actor' },
+      }
+  },
+  movies_rates: {
+    type: 'one-to-many',
+    target: 'rating',
+    inverseSide: 'Movie',
+},
+  }
+}
+);
 
-export default MovieDB;
+export default Movie;
