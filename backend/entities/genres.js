2d6248d2390712fc833479f654e6f0666aa5a62d
Houssem Guermazi <houssem.guermazi@student-cs.fr>
2024-06-05 14:56:44 +0200

    test

---
+++ backend/entities/genres.js
@@ -0,0 +1,26 @@
+import typeorm from 'typeorm';
+
+const Genre = new typeorm.EntitySchema({
+  name: 'Genre',
+  columns: {
+    id_genre: {
+      primary: true,
+      type: Number,
+      generated: true,
+    },
+    genre_type: { type: String }
+    
+  },
+  relations: {
+    movie_genre: {
+      type: 'many-to-many',
+      target: 'Movie',
+      joinTable: {
+          name: 'movie_genre', // Name of the join table
+          joinColumn: { name: 'id_genre', referencedColumnName: 'id_genre' },
+          inverseJoinColumn: { name: 'id_movie', referencedColumnName: 'id_movie' },
+      }
+    }},
+});
+
+export default Genre;
