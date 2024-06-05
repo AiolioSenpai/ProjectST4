777fd10c4ca291ea044a794d4e9cfecacc9c5e78
El Bahloul El Hassan <el-hassan.el-bahloul@student-cs.fr>
2024-06-05 16:44:56 +0200

    dedicated movie page update

--- frontend/src/pages/movieDetails/movieDetails.jsx
+++ frontend/src/pages/movieDetails/movieDetails.jsx
@@ -21,7 +21,21 @@ function MovieDetails() {
     <div className="movie-details">
             <h1 className="movie-title">{movie.title}</h1>
             <div className="movie-content">
+                <div className='movie-media'>
                 <img className="movie-poster" src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`} alt={movie.title} />
+                <div className="movie-trailer">
+                        <h3>Trailer :</h3>
+                        <iframe
+                            width="1024"
+                            height="720"
+                            src={`https://www.youtube.com/embed/PLl99DlL6b4`} //${movie.trailer_key}
+                            title="YouTube video player"
+                            frameBorder="0"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                            allowFullScreen
+                        ></iframe>
+                    </div>
+                </div>
                 <div className="movie-info">
                     <p className="movie-overview">{movie.overview}</p>
                     <p className="movie-release-date">Date de sortie : {movie.release_date}</p>
@@ -33,18 +47,14 @@ function MovieDetails() {
                             ))}
                         </ul>
                     </div> */}
-                    <div className="movie-trailer">
-                        <h3>Trailer :</h3>
-                        <iframe
-                            width="560"
-                            height="315"
-                            src={`https://www.youtube.com/embed/PLl99DlL6b4`} //${movie.trailer_key}
-                            title="YouTube video player"
-                            frameBorder="0"
-                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                            allowFullScreen
-                        ></iframe>
-                    </div> 
+                    <div className="movie-genres">
+                        <div className="genres-list">
+                        <h3 className='genres-word'>Genres :</h3>
+                            {movie.genre_ids.map((genre, index) => (
+                                <span key={index} className="genre-tag">{genre}</span>
+                            ))}
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
